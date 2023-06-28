abb44d1003de5f6605a272195eacad2f6bdcf70a
Anoopinnow8 <apatal@innow8apps.com>
2023-06-28 14:38:26 +0530

    update

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -3,7 +3,7 @@ import "./style.scss";
 import { useNavigate } from "react-router-dom";
 import useFetch from "../../../hooks/useFetch";
 import { useSelector } from "react-redux";
-import Img from "../../../components/LazyLoadImage/img";
+// import Img from "../../../components/LazyLoadImage/img";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 
 const HeroBanner = () => {
@@ -31,9 +31,7 @@ const HeroBanner = () => {
   return (
     <div className="heroBanner">
       {!loading && (
-        <div className="backdrop-img">
-          <Img src={background} />
-        </div>
+        <div className="backdrop-img">{/*<Img src={background} />*/}</div>
       )}
 
       <div className="opacity-layer"></div>
