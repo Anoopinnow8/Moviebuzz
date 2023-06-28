e682b88f25a2296574db3e28a37f7bd7369e8e92
Anoop <apatel@innow8apps.com>
2023-06-28 10:55:18 +0530

    moviebuzz Project code

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Welcome to our movie website, where we bring you the latest updates,
+          reviews, and information about the world of cinema. Our passion for
+          movies drives us to provide you with a comprehensive platform to
+          explore a wide range of films, from blockbusters to indie gems. We
+          strive to be your go-to source for all things related to the silver
+          screen, offering an engaging and immersive experience for movie
+          enthusiasts like you
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import "./style.scss";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../../../hooks/useFetch";
+import { useSelector } from "react-redux";
+import Img from "../../../components/LazyLoadImage/img";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+
+const HeroBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home);
+
+  const { data, loading } = useFetch("/movie/upcoming");
+
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
+
+  const searchQueryHandler = (event) => {
+    event.preventDefault();
+    if (query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+
+  return (
+    <div className="heroBanner">
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
+        </div>
+      )}
+
+      <div className="opacity-layer"></div>
+      <ContentWrapper>
+        <div className="heroBannerContent">
+          <span className="title">Welcome</span>
+          <span className="subTitle">
+            Welcome Discover the millions of movies,shows
+          </span>
+          <div className="searchInput">
+            <form onSubmit={(e) => searchQueryHandler(e)}>
+              <input
+                type="text"
+                placeholder="Search Your Movie"
+                onChange={(e) => setQuery(e.target.value)}
+              />
+
+              <button>Search </button>
+            </form>
+          </div>
+        </div>
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default HeroBanner;
