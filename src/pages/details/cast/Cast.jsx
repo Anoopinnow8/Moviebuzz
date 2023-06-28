911ce2b103bd7b0a36f79c0aa0c188e2d21f0406
Anoopinnow8 <apatal@innow8apps.com>
2023-06-28 12:38:12 +0530

    code push1

--- src/pages/details/cast/Cast.jsx
+++ src/pages/details/cast/Cast.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import "./style.scss";
 
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import Img from "../../../components/lazyLoadImage/Img";
+import Img from "../../../components/lazyLoadImage/img";
 import avatar from "../../../assets/Moviex-images/avatar.png";
 
 const Cast = ({ data, loading }) => {
